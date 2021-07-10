6acd9913257614d792a3ca207e2cd80abb5d34ea
Florian Richter <flori@hey.com>
2021-07-10 15:48:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
+const { readFileSync } = require("fs");
+const { spawnSync } = require("child_process");
+
+const webhooks = new Webhooks({
+  secret: process.env.SECRET,
+});
+
+const configurations = JSON.parse(
+  readFileSync("config.json", { encoding: "utf-8" })
+);
+
+webhooks.on("push", ({ payload }) => {
+  console.log("Event received!");
+  console.log("Ref:", payload.ref);
+  console.log("Repo:", payload.repository.full_name);
+
+  const config = configurations.find(
+    (c) => c.repository === payload.repository.full_name
+  );
+
+  if (
+    config &&
+    payload.repository.default_branch === payload.ref.split("/")[2]
+  ) {
+    console.log("Pulling");
+    spawnSync("git", ["pull"], config.cwd);
+
+    for (const { command, args, cwd } of config.commands) {
+      console.log(`Executing ${command} ${args.join(" ")} at ${cwd}`);
+      spawnSync(command, args, { cwd });
+    }
+  }
+});
+
+require("http")
+  .createServer(createNodeMiddleware(webhooks))
+  .listen(process.env.PORT || 8080);
