950af6d157000b6e1cfdf1d9f6b6efce4a73e534
Florian Richter <flori@hey.com>
2021-11-17 15:26:31 +0100

    Add downloadArtifact

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
-const { readFileSync } = require("fs");
+const { request } = require("@octokit/request");
+const { readFileSync, writeFileSync } = require("fs");
 const cp = require("child_process");
 const nodemailer = require("nodemailer");
 const { promisify } = require("util");
+
+const requestWithAuth = request.defaults({
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
+});
 
 const exec = promisify(cp.exec);
 
@@ -14,26 +21,50 @@ const configurations = JSON.parse(
   readFileSync("config.json", { encoding: "utf-8" })
 );
 
-webhooks.on("push", async ({ payload }) => {
+for (const config of configurations) {
+  webhooks.on(config.on, processEvent);
+}
+
+async function processEvent({ payload }) {
   console.log("Event received!");
   console.log("Repo:", payload.repository.full_name);
 
-  const config = configurations.find(
-    (c) => c.repository === payload.repository.full_name
-  );
-
   if (
-    config &&
-    payload.repository.default_branch === payload.ref.split("/")[2]
+    config.repository === payload.repository.full_name &&
+    (payload.repository.default_branch === payload.ref?.split("/")[2] ||
+      payload.repository.default_branch === payload.workflow_run?.head_branch)
   ) {
-    console.log("Pulling");
-    await exec("git pull", { cwd: config.cwd });
-
     try {
       for (const { command, cwd, timeout } of config.commands) {
-        console.log(`Executing ${command} at ${cwd}`);
-        await exec(command, { cwd, timeout });
-        await sleep(10);
+        if (command === "downloadArtifact") {
+          const res = await requestWithAuth(
+            "GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts",
+            {
+              owner: payload.repository.owner.login,
+              repo: payload.repository.name,
+              run_id: payload.workflow_run.id,
+            }
+          );
+          for (const artifact of res.data.artifacts) {
+            const artifactRes = await requestWithAuth(
+              "GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}",
+              {
+                owner: payload.repository.owner.login,
+                repo: payload.repository.name,
+                artifact_id: artifact.id,
+                archive_format: "zip",
+              }
+            );
+            writeFileSync(
+              path.resolve(cwd, "build.zip"),
+              Buffer.from(artifactRes.data)
+            );
+          }
+        } else {
+          console.log(`Executing ${command} at ${cwd}`);
+          const timeoutInMinutes = timeout * 1000 * 60;
+          await exec(command, { cwd, timeout: timeoutInMinutes });
+        }
       }
       console.log("All done!");
     } catch (err) {
@@ -51,20 +82,13 @@ webhooks.on("push", async ({ payload }) => {
       console.error("Build failed");
     }
   }
-});
+}
 
 async function createTransport() {
   return nodemailer.createTransport({
     host: "localhost",
     port: 25,
     tls: { servername: "6v4.de" },
-  });
-}
-
-async function sleep(seconds) {
-  const ms = seconds * 1000;
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
   });
 }
 
